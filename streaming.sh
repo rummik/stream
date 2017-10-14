35c728b8669e33e8313409cb407bb7930e6da8cd
rummik <k@9k1.us>
2017-10-14 09:11:03 -0400

    Updates

--- streaming.sh
+++ streaming.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
-streamkeys &
+#!/bin/bash
 
-function running {
+running() {
 	ps ax | grep $1 | grep -v grep > /dev/null
 }
 
-if ! running mplayer; then
-	mplayer -fps 3 -tv quality=10:driver=v4l2:width=640:height=480:fps=5:outfmt=i420:device=/dev/video0 -vo caca tv:// &
+if ! running screenkey; then
+	screenkey &
+fi
+
+if ! running vlc; then
+	vlc -I dummy 'v4l2:///dev/video1:fps=12:width=640:height=400' --video-filter='adjust{contrast=1.7}' -V aa
 fi
 
 if ! running obs; then
-	obs &
+	obs
 fi
